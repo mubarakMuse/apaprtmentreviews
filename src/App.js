51bd234d5238e3515bb67fd243b6c3b2696e5cf6
mubarak muse <mubarakmuse@mubaraks-MacBook-Pro.local>
2023-08-23 14:54:46 -0500

    v2

--- src/App.js
+++ src/App.js
@@ -2,23 +2,29 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import ReviewList from './ReviewList';
 import ReviewForm from './ReviewForm';
-import { Navbar, Nav } from 'react-bootstrap';
+import HomePage from './HomePage'; // Import the HomePage component
+import { Navbar } from 'react-bootstrap';
+import spinningImage from './rms-logo.png'; // Replace with your spinning image
 
 function App() {
   return (
     <Router>
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand as={Link} to="/">Apartment Reviews</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Link as={Link} to="/submit">Submit Review</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+      <Navbar bg="white" expand="lg">
+        <Navbar.Brand as={Link} to="/">
+          <img
+            src={spinningImage} // Replace with the actual path to your logo image
+            width="100"
+            height="50"
+            className="d-inline-block align-top"
+            alt="Review My Spot Logo"
+          />
+        </Navbar.Brand>
+        {/* ... Rest of the Navbar code ... */}
       </Navbar>
       <Routes>
-        <Route path="/" element={<ReviewList />} />
+        <Route path="/" element={<HomePage />} /> {/* Set the HomePage as the root route */}
         <Route path="/submit" element={<ReviewForm />} />
+        <Route path="/search" element={<ReviewList />} />
       </Routes>
     </Router>
   );
---
+++ src/HomePage.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Container } from 'react-bootstrap';
+import spinningImage from './rms-logo.png'; // Replace with your spinning image
+import './styles.css';
+
+function HomePage() {
+  return (
+    <Container className="d-flex flex-column align-items-center justify-content-center" style={{ minHeight: '80vh' }}>
+      <img src={spinningImage} alt="Spinning" width="300" height="200" className="mb-4" />
+
+      <h4 className="text-center font-weight-bold mb-4">#1 Database for Apartment Reviews</h4>
+      <div className="d-flex flex-column align-items-center">
+        <Button as={Link} to="/submit" variant="primary" className="mb-3">
+        Rate Your Apartment
+        </Button>
+        <Button as={Link} to="/search" variant="secondary">
+        Find Apartment Reviews
+        </Button>
+      </div>
+    </Container>
+  );
+}
+
+export default HomePage;
